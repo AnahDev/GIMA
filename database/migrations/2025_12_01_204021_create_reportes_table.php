cec4ecce82442c9ea00110f6cedc0471e0b5555f
maikol2001 <miguelenriquegm80@gmail.com>
2025-12-02 18:44:24 -0400

    hice la tabla de mantenimiento y reportes

---
+++ database/migrations/2025_12_01_204021_create_reportes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reportes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('users');
+            $table->foreignId('activo_id')->constrained('activos');
+            $table->string('descripcion');
+            $table->integer('prioridad');
+            $table->integer('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reportes');
+    }
+};

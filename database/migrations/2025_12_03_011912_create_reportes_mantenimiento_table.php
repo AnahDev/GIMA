19203f02176414b4f05d6f554c8a05dd83b77284
Hunt440 <smash.kamper@gmail.com>
2025-12-05 14:01:44 -0400

    Cesar reyes, añadi las migraciones sesiones_mantenimiento, repuestos_usados y reportes_mantenimiento

---
+++ database/migrations/2025_12_03_011912_create_reportes_mantenimiento_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reportes_mantenimiento', function (Blueprint $table) {
+            $table->id();
+            $table->mantenimiento_id()->foreignId()->constrained('mantenimientos');
+            $table->reporte_id()->foreignId()->constrained('reportes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reportes_mantenimiento');
+    }
+};

02184ef7cc517192958853e173034ad04ebf277c
DavFer023 <davferez023@gmail.com>
2026-01-06 21:18:02 -0400

    resources

--- app/Models/Mantenimiento.php
+++ app/Models/Mantenimiento.php
@@ -65,7 +65,7 @@ class Mantenimiento extends Model
         return $this->hasMany(SesionesMantenimiento::class, 'mantenimiento_id');
     }
     // Relación: Un mantenimiento pertenece a un reporte
-    p`ublic function reporte(): BelongsTo
+    public function reporte(): BelongsTo
     {
         return $this->belongsTo(Reporte::class, 'reporte_id');
     }

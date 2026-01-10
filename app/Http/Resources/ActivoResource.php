872ac4153d56f6daef5115acbc8abbce1d21dea3
AnahDev <anadevtools@gmail.com>
2026-01-09 20:44:56 -0400

    Archivos Resource Agregados

---
+++ app/Http/Resources/ActivoResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ActivoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array. 
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'estado' => $this->estado, // Laravel serializa el Enum automáticamente
+            'valor' => $this->valor,
+            // Relaciones: Solo se cargan si están presentes para optimizar rendimiento
+            'articulo' => new ArticuloResource($this->whenLoaded('articulo')),
+            'ubicacion' => new UbicacionResource($this->whenLoaded('ubicacion')),
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
